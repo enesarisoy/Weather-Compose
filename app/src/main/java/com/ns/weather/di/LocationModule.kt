a22a8c1ed5b76120f576b387c6f3e48d05aa8de2
enesarisoy <arisoyeness@gmail.com>
2023-03-14 19:52:05 +0300

    hilt setup

---
+++ app/src/main/java/com/ns/weather/di/LocationModule.kt
@@ -0,0 +1,18 @@
+package com.ns.weather.di
+
+import com.ns.weather.data.location.DefaultLocationTracker
+import com.ns.weather.domain.location.LocationTracker
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker(defaultLocationTracker: DefaultLocationTracker): LocationTracker
+}
---
+++ app/src/main/java/com/ns/weather/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.ns.weather.di
+
+import com.ns.weather.data.repository.WeatherRepositoryImpl
+import com.ns.weather.domain.repository.WeatherRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(
+        weatherRepositoryImpl: WeatherRepositoryImpl
+    ): WeatherRepository
+}
