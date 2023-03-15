21e63301d3f798b02b38144e23bf9bb3b640e87c
enesarisoy <arisoyeness@gmail.com>
2023-03-15 11:42:21 +0300

    forecast

--- app/src/main/java/com/ns/weather/MainActivity.kt
+++ app/src/main/java/com/ns/weather/MainActivity.kt
@@ -8,11 +8,16 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.ns.weather.presentation.WeatherCard
+import com.ns.weather.presentation.WeatherForecast
 import com.ns.weather.presentation.WeatherViewModel
 import com.ns.weather.presentation.ui.theme.DarkBlue
 import com.ns.weather.presentation.ui.theme.DeepBlue
@@ -41,12 +46,33 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             WeatherTheme {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(DarkBlue)
-                ) {
-                    WeatherCard(state = viewModel.state, backgroundColor = DeepBlue)
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(DarkBlue)
+                    ) {
+                        WeatherCard(state = viewModel.state, backgroundColor = DeepBlue)
+                        Spacer(modifier = Modifier.height(16.dp))
+                        WeatherForecast(state = viewModel.state)
+                    }
+
+                    if (viewModel.state.isLoading) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.align(Alignment.Center),
+                            color = Color.White
+                        )
+                    }
+                    viewModel.state.error?.let { error ->
+                        Text(
+                            text = error,
+                            color = Color.White,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.align(
+                                Alignment.Center
+                            )
+                        )
+                    }
                 }
             }
         }
